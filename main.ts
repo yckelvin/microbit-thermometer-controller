e21dab5afa3a406b5e6ab4fe96d22d23f2d334cb
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-07-25 21:24:31 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 function API (method: string, degree: number) {
-    if (method.compare("GET") == 0) {
-        radio.sendString("GET")
-        basic.showString("G")
-    } else if (method.compare("POST") == 0) {
+    if (method == "GET") {
+        radio.sendValue("GET", degree)
+    } else {
         radio.sendValue("POST", degree)
-        basic.showString("P")
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -14,13 +12,14 @@ input.onButtonPressed(Button.B, function () {
     API("POST", randint(0, 25))
 })
 radio.onReceivedValue(function (name, value) {
-    if ("returnTemp".compare(name) == 0) {
+    if (name == "Temp") {
         Temp = value
         basic.showNumber(Temp)
     }
 })
 let Temp = 0
-radio.setGroup(120)
+radio.setGroup(5)
 radio.setTransmitSerialNumber(true)
 radio.setTransmitPower(7)
 Temp = 0
+basic.showString("Ready")
